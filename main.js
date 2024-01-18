9854411b5c47ddf66b926219af3a281100338299
Isaprogramacion <133288873+Isaprogramacion@users.noreply.github.com>
2024-01-18 18:50:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+noseX=0;
+noseY=0;
+
+function preload() { }
+
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    }
+
+function modelLoaded() {
+
+}
+
+function gotPoses(results)
+{
+ 
+  
+}
+
+function draw() {
+  
+ 
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png')
+}
